29b7a122f3a5002578caa619876907ca26bb1ded
antoniopropla7e <xavierliduena08@gmail.com>
2025-10-06 11:42:38 -0400

    Add files via upload

---
+++ Tarea1.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Cual es el primer numero?:"))
+numero2 = int(input("Cual es el segundo numero?:"))
+suma = numero1 + numero2
+resta = numero1 - numero2
+multiplicacion = numero1 * numero2
+division = numero1 / numero2
+print (suma)

6d910eb6d210073b58f0f7feef46855f956fa86c
antoniopropla7e <xavierliduena08@gmail.com>
2025-10-07 11:53:39 -0400

    Add files via upload

---
+++ el_diccionario = {.py
@@ -0,0 +1,23 @@
+el_diccionario = {
+    "nombre":"Xavier",
+    "edad": 17,
+    "ciudad": "Maracaibo"}
+    print("\nvalor:",valor)
+    print(f"Valores aqui: {valor}")
+    print(f"\nClave: {clave}, Valor: {valor}")
+
+contador =0
+
+while contador < 50:
+    print(f"Contador: {contador}")
+    contador += 1
+
+for i in range (7):
+    print(F"iteracion {i+ 1}")
+    calificacion = 101
+if calificacion < 100:
+    print("meh")
+elif calificacion == 100:
+    print("bien!")
+else:
+    print("cuanto sacaste?")
